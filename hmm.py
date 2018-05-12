0ebe13a9422257aee0ced26b4b64258067d9ee2a
kristinablaskova <blaskova.kristina@gmail.com>
2018-05-12 11:34:14 +0200

    handled not starting with wake state and missing labeled data column + start dropping NotScored data

--- hmm.py
+++ hmm.py
@@ -8,55 +8,68 @@ from dist import Distributions
 
 
 def run_hmm_on_files(path, n_features):
-    print(path)
+    poznamka = []
+    try:
+        print(path)
 
-    data, n_features, feature_names = preprocess_any_file(path, n_features)
-
-
-    def preprocess_data(data):
-        train, test = train_test_split(data, test_size=0.0, shuffle=False)
-        data_columns = list(data.columns.values)
-        hidden_sequence = data['hypnogram_User'].tolist()
-        for i in range(0, len(hidden_sequence)):
-            if hidden_sequence[i] == "NotScored":
-                hidden_sequence[i] = hidden_sequence[i - 1]
-
-        observation_sequence = train.iloc[:, 0:n_features].values.tolist()
-        return data_columns, hidden_sequence, observation_sequence, train, test
-
-    def create_states(model, hidden_sequence, state_names):
-        chain_model = pg.MarkovChain.from_samples([hidden_sequence])
-        states = {}  # type: Dict[str, pg.State]
-        for name in state_names:
-            states[name] = pg.State(dist[state_names.index(name)], name=name)
-        model.add_states(list(states.values()))
-        # sets the starting probability for state 'Wake' to 1.0
-        model.add_transition(model.start, states['Wake'], 1.0)
-        # insert the emission probabilities, that we computed in summary
-        for prob in chain_model.distributions[1].parameters[0]:
-            state1 = states[prob[0]]
-            state2 = states[prob[1]]
-            probability = prob[2]
-            model.add_transition(state1, state2, probability)
+        data, n_features, feature_names = preprocess_any_file(path, n_features)
 
 
+        def preprocess_data(data):
+            train, test = train_test_split(data, test_size=0.0, shuffle=False)
+            data_columns = list(data.columns.values)
+            hidden_sequence = data['hypnogram_User'].tolist()
+            l = len(hidden_sequence)
+            for i in reversed(range(0, l)):
+                if hidden_sequence[i] == "NotScored":
+                    train = train.drop([i])
+                    del hidden_sequence[i]
+            observation_sequence = train.iloc[:, 0:n_features].values.tolist()
+            return data_columns, hidden_sequence, observation_sequence, train, test
 
-    data_columns, hidden_sequence, observation_sequence, train, test = preprocess_data(data)
-    hmm_dist = Distributions(train)
-    dist, state_names = hmm_dist.gauss_kernel_dist(feature_names)
-    model = pg.HiddenMarkovModel('prediction')
-    create_states(model, hidden_sequence, state_names)
-    model.bake()
+        def create_states(model, hidden_sequence, state_names):
+            chain_model = pg.MarkovChain.from_samples([hidden_sequence])
+            states = {}  # type: Dict[str, pg.State]
+            for name in state_names:
+                states[name] = pg.State(dist[state_names.index(name)], name=name)
+            model.add_states(list(states.values()))
+            # sets the starting probability for state 'Wake' to 1.0
+            try:
+                model.add_transition(model.start, states['Wake'], 1.0)
+                poznamka.append("")
+            except KeyError:
+                print("nezacina wake")
+                poznamka.append('nezacina wake')
+                pass
+            # insert the emission probabilities, that we computed in summary
+            for prob in chain_model.distributions[1].parameters[0]:
+                state1 = states[prob[0]]
+                state2 = states[prob[1]]
+                probability = prob[2]
+                model.add_transition(state1, state2, probability)
 
-    test_observation_sequence = train.iloc[:, 0:n_features].values.tolist()
-    #hmm_fit = model.fit([observation_sequence], labels=[hidden_sequence], algorithm='labeled')
-    hmm_pred = model.predict(test_observation_sequence)
 
-    conf_hmm = confusion_matrix(hidden_sequence, [state_names[id] for id in hmm_pred], state_names)
-    #print(conf_hmm)
-    #print(state_names)
 
-    state_ids = np.array([state_names.index(val) for val in hidden_sequence])
-    score = (np.array(hmm_pred) == state_ids).mean()
-    print(score)
-    return score
+        data_columns, hidden_sequence, observation_sequence, train, test = preprocess_data(data)
+        hmm_dist = Distributions(train)
+        dist, state_names = hmm_dist.gauss_kernel_dist(feature_names)
+        model = pg.HiddenMarkovModel('prediction')
+        create_states(model, hidden_sequence, state_names)
+        model.bake()
+
+        test_observation_sequence = train.iloc[:, 0:n_features].values.tolist()
+        #hmm_fit = model.fit([observation_sequence], labels=[hidden_sequence], algorithm='labeled')
+        hmm_pred = model.predict(test_observation_sequence)
+
+        conf_hmm = confusion_matrix(hidden_sequence, [state_names[id] for id in hmm_pred], state_names)
+        #print(conf_hmm)
+        #print(state_names)
+
+        state_ids = np.array([state_names.index(val) for val in hidden_sequence])
+        score = (np.array(hmm_pred) == state_ids).mean()
+        print(score)
+    except ValueError:
+        print('nejaky valueerror - napr nepozna stlpec hypnogram user')
+        score = "NaN"
+
+    return poznamka, score
--- run_model_all_data.py
+++ run_model_all_data.py
@@ -1,23 +1,16 @@
 import os
 from hmm import run_hmm_on_files
 import pandas as pd
-directory = os.fsencode('/Users/kristina/PycharmProjects/vyskumak/Data')
+directory = os.fsencode('/Users/kristina/PycharmProjects/vyskumak/Data_hradec')
 
-experimental_df1 = pd.read_csv('/Users/kristina/PycharmProjects/vyskumak/experimenty/score_EEGonly_noTrainTest.csv')
-experimental_df1 = experimental_df1.drop('Unnamed: 0', axis=1)
-experimental_df1 = experimental_df1.rename(index=str, columns={"skore": "skore_gauss"})
-experimental_df = pd.DataFrame(columns=['gauss_kernel_binar'])
-
-index=0
+experimental_df = pd.DataFrame(columns=['file', 'vysledok', 'poznamka'])
 for file in os.listdir(directory):
 
     filename = os.fsdecode(file)
     if filename.endswith(".csv"):
         path =str(directory)[2:-1]+"/"+str(filename)
-        score = run_hmm_on_files(path, n_features=10)
-        experimental_df = experimental_df.append({'gauss_kernel_binar': score}, ignore_index=True)
+        poznamka, score = run_hmm_on_files(path, n_features=10)
+        experimental_df = experimental_df.append({'file': filename, 'vysledok': score, 'poznamka': poznamka}, ignore_index=True)
 
-experimental_df1= experimental_df1.reset_index(drop=True)
 experimental_df= experimental_df.reset_index(drop=True)
-experimental_df1 = experimental_df1.join(experimental_df)
-experimental_df1.to_csv('1score_EEGonly_noTrainTest.csv', sep = ',')
+experimental_df.to_csv('experimenty/olomouc_experiment_gausskernel.csv', sep = ',')
